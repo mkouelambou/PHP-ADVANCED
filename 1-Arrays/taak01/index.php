f663082b2bfaa303d89e85ae0ea3868ea12d022a
Mike <mike.kouelambou@student.rocva.nl>
2021-04-01 11:49:21 +0200

    save changes

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,9 @@
+<?php
+$voornaam = array("Piet", "Muhammad", "Els");
+$achternaam = array("Van Vugt", "Ali", "Van Dijk",);
+$leeftijd = array("16", "17", "18",);
+$klas = array("9A", "9B", "9C",);
+echo $voornaam[0];
+echo $leeftijd[0];
+echo $klas[0];
+?>
