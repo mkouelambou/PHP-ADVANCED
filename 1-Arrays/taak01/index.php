fe1cd9aeb8f7c9b6879b9af155f4f855e1147cbd
Mike <mike.kouelambou@student.rocva.nl>
2021-04-01 13:36:21 +0200

    save change

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -1,9 +1,20 @@
 <?php
-$voornaam = array("Piet", "Muhammad", "Els");
-$achternaam = array("Van Vugt", "Ali", "Van Dijk",);
-$leeftijd = array("16", "17", "18",);
-$klas = array("9A", "9B", "9C",);
-echo $voornaam[0];
-echo $leeftijd[0];
-echo $klas[0];
+$student1 = ["voornaam" => "Piet", "achternaam" => "Van Vugt", "leeftijd" => "16", "klas" => "9A"];
+$student2 = ["voornaam" => "Muhammad", "achternaam" => "Ali", "leeftijd" => "17", "klas" => "9B"];
+$student3 = ["voornaam" => "Els", "achternaam" => "Van Dijk", "leeftijd" => "18", "klas" => "9C"];
+echo $student1["voornaam"];
+echo $student1["achternaam"];
+echo $student1["leeftijd"];
+echo $student1["klas"];
+
+echo $student2["voornaam"];
+echo $student2["achternaam"];
+echo $student2["leeftijd"];
+echo $student2["klas"];
+
+echo $student3["voornaam"];
+echo $student3["achternaam"];
+echo $student3["leeftijd"];
+echo $student3["klas"];
+
 ?>
