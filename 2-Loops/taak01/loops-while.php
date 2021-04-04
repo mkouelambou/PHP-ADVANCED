91c3b3b01ef8edf24ec9b590289b5189bab19c9d
Mike <mike.kouelambou@student.rocva.nl>
2021-04-04 16:39:07 +0200

    save changes

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$leeftijd = 0;
+while($leeftijd < 18){
+    echo "<li><ul> Ik ben $leeftijd jaar oud, dus ik mag nog niet stemmen</li></ul>";
+    $leeftijd++;
+}
+echo "<li><ul> Ik ben 18 jaar oud, dus ik heb stemrecht!</li></ul>";
+?>   
+</body>
+</html>
