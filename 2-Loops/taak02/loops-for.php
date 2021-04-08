868040b8c5c19c84335c7447c23b833e89479582
Mike <mike.kouelambou@student.rocva.nl>
2021-04-08 12:06:32 +0200

    save code

--- 2-Loops/taak02/loops-for.php
+++ 2-Loops/taak02/loops-for.php
@@ -9,8 +9,9 @@
 <body>
 <?php
 $leeftijd = 0;
-for($leeftijd = 0; $leeftijd < 18; $leeftijd++;){
+for($leeftijd = 0; $leeftijd < 18;){
     echo "<li><ul> Ik ben $leeftijd jaar oud, dus ik mag nog niet stemmen</li></ul>";
+    $leeftijd++;
 }
     echo "<li><ul> Ik ben 18 jaar oud, dus ik heb stemrecht!</li></ul>";
 ?>   
