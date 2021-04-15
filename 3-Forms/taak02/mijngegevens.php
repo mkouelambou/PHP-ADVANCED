269b2302d1896b7e93f5b919f4e3a8758c1daf57
Mike <mike.kouelambou@student.rocva.nl>
2021-04-15 10:53:44 +0200

    SAVE CODE

---
+++ 3-Forms/taak02/mijngegevens.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo $_POST['gender'];
+    echo "<br>";
+    echo $_POST['voornaam'];
+    echo "<br>";
+    echo $_POST['achternaam'];
+    echo "<br>";
+    echo $_POST['klas'];
+    echo "<br>";
+    echo $_POST['leeftijd'];
+    echo "<br>";
+    echo $_POST['adres'];
+    echo "<br>";
+    echo $_POST['plaatsnaam'];
+    echo "<br>";
+    echo $_POST['muziekband'];
+    ?>
+</body>
+</html>
